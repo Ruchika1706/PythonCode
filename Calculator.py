7fb7d557bda9d28b6a863c49f5971beead63d80c
Ruchika1706 <luthraruchika19@gmail.com>
2017-03-22 20:04:44 +0530

    Basic Mathematical operations in Python

---
+++ Calculator.py
@@ -0,0 +1,28 @@
+#Basic Calculations in Python
+a = 5
+b = 4
+print(a+b) #Addition
+print(a-b) #subtraction
+print(a*b) #multiplication
+print(a/b) #division
+print(a**b) #Exponent
+print(a%b) #Remainder or modulus
+print(a//3) #Quotient
+print(max(a,b)) #Maximum of two numbers
+print(min(a,b)) #Minimum of two numbers
+print(a>b) #relational Operator
+print(a<b)
+print(a==b)
+print(not(a))
+print(a!=b)
+print(a>=b)
+print(a<=b)
+print(not(not(a)))
+print(a and b) #logical operator
+print(a or b)
+print(a & b) #Bitwise Operator
+print(a | b)
+print(~a)
+print(a is True) #is or isnot are identity operations in Python.To check if two values are located on the same part of memory
+print(a is not True)
+print(a is 5)
