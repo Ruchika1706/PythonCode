53acaf011c5fb48804714612541591ef045b9156
Ruchika1706 <luthraruchika19@gmail.com>
2017-04-01 19:40:31 +0530

    immutable tuples

---
+++ Tuples.py
@@ -0,0 +1,7 @@
+fruits = ('Apple','Mango', 'Peach')
+print(fruits)
+print(fruits[0])
+print(fruits[1])
+print(fruits[2])
+# Tuples are immutable. It will cause  TypeError and does not support item assignment
+fruits[0] = "Banana"
