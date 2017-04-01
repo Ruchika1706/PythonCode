d9e179f2950fb8b9375d8563104e88a7cf993044
Ruchika1706 <luthraruchika19@gmail.com>
2017-04-01 15:53:27 +0530

    Basic usage for loop

---
+++ for_loop.py
@@ -0,0 +1,15 @@
+#print numbers from 1 to 10 using for loop
+for each in range(1,11):
+    print(each,"Ruchika")
+
+fruits = ["Apple", "banana", "Mango", "Peach"]
+for each in fruits:
+    print each
+
+#print even numbers between 1 to 20
+for each in range(1,21):
+    if each%2==0:
+        print each
+
+for each in range(0,21,2):
+    print each
