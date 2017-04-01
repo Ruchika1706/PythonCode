0e6cce37dcc33ad176e31e2c463b0cc3a56cd719
Ruchika1706 <luthraruchika19@gmail.com>
2017-04-01 14:41:54 +0530

    Usage of if else and elif

---
+++ AdultOrNot.py
@@ -0,0 +1,14 @@
+#Usage of conditional expression in python
+age = int(raw_input("Enter your age"))
+if (age>=18):
+    print("You are adult")
+else:
+    print("You are not an adult")
+
+marks = int(raw_input("Enter your marks"))
+if marks >= 90:
+    print('Grade A')
+elif marks >=70:
+    print('Grade B')
+else:
+    print("Grade C")
