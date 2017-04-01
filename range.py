ae27dea925b1f37041a8fb0ba096fa5bab9c12fe
Ruchika1706 <luthraruchika19@gmail.com>
2017-04-01 15:54:21 +0530

    usage and variation of range() in python

---
+++ range.py
@@ -0,0 +1,15 @@
+numbers = list(range(11))
+print(numbers)
+
+numbers = list(range(3,8))
+print(numbers)
+
+numbers = list(range(1,30,3))
+print(numbers)
+
+numbers = list(range(30,1,-3))
+print(numbers)
+
+
+numbers = range(-30,-1,3)
+print(numbers)
