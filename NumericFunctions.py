f0e7903f259e6e41e46d01cbec2be6e0df301d82
Ruchika1706 <luthraruchika19@gmail.com>
2017-04-01 21:34:04 +0530

    Quiz Questions

--- NumericFunctions.py
+++ NumericFunctions.py
@@ -7,3 +7,15 @@ print(max(3,4,1,2,4))
 
 #absolute value function: Core value without including sign of the value
 print(abs(-203))
+
+#products and their prices
+product_list = {'car':900000, 'bike':700000,'cycle':10000,'scooty':45000,'auto':25000}
+product = raw_input("Enter the product name")
+if product in product_list:
+    print(product_list.get(product,"Product found"))
+else:
+    print("product not found")
+
+#List out  all the odd numbers from 1 to 100 using lists in Python.
+list = [num for num in range(1,100) if num%2 != 0]
+print list
