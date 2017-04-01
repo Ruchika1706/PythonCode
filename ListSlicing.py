320a9b11292535c06493c3c337ce921fc653b1c8
Ruchika1706 <luthraruchika19@gmail.com>
2017-04-01 20:41:05 +0530

    List Slicing Operations

---
+++ ListSlicing.py
@@ -0,0 +1,16 @@
+numbers = [100, 200, 300, 400, 500, 600]
+#print the list
+print(numbers)
+#print the entire list
+print(numbers[:])
+#print list from index 1 till 3, not including 3
+print(numbers[1:3])
+#print list from index 1 till last index, last index included
+print(numbers[1:])
+#print list from index first till given index, zeroth index included
+print(numbers[:4])
+
+
+#specifying the interval
+#print the entire list with interval of 2
+print(numbers[::2])
