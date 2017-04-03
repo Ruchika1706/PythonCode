e401181d60c9cfe77fa683b069af5ad250e03123
Ruchika1706 <luthraruchika19@gmail.com>
2017-04-03 12:17:45 +0530

    Adding one more example

--- OperatorOverloading.py
+++ OperatorOverloading.py
@@ -13,3 +13,33 @@ class overloading:
 test = overloading(3,4)
 drive = overloading(5,6)
 manual = test+drive
+
+#Operator Overloading : Overload a particular operator to work with different data types
+#Add coordinates of two places (x1,y1) and (x2,y2)
+class Point:
+    def __init__(self,x,y):
+        self.x = x
+        self.y = y
+    def getitem(self):
+        self.x = int(raw_input("Enter the value of x coorindate"))
+        self.y = int(raw_input("Enter the value of y coorindate"))
+
+    def putitem(self):
+        print("(x,y)"+"("+self.x+","+self.y+")")
+
+    def __add__(self,other):
+        x = self.x + other.x
+        y = self.y + other.y
+        return Point(x,y)
+
+    def __sub__(self,other):
+        x = self.x - other.x
+        y = self.y - other.y
+        return Point(x, y)
+
+    def __str__(self):
+        return "({0},{1})".format(self.x,self.y)
+
+a1 = Point(3,4)
+a2 = Point(5,6)
+print(a1 + a2)
