f6427739f9fad6dde4bceaaf8eaeec64f44aaf70
Ruchika1706 <luthraruchika19@gmail.com>
2017-04-03 15:09:02 +0530

    Adding comments

--- Filters.py
+++ Filters.py
@@ -1,4 +1,5 @@
 #Filters are used along with Lambda function to filter out certain elements from a list
 new_list = [10,20,21,31,40,51]
+#Filter out only even numbers present in the given list.
 result = list(filter(lambda x: x % 2 == 0, new_list))
 print result
